eb371e61952557511d7b8612c1760d9a6d7380ff
Elizabeth Hau <elizabeth.h.hau@gmail.com>
2022-05-07 09:44:14 -0400

    start listening for skip as soon as a pose has been loaded

--- templates/static/js/setupSpeech.js
+++ templates/static/js/setupSpeech.js
@@ -36,8 +36,8 @@ const processSpeech = function (transcript) {
   //}
   else if (!hasLoadedPose) {
     processed = loadPoseImage(transcript);
-  // } else if (transcript.includes('skip')) {
-  } else if (coachFetchedCount > 2) {
+  } else if (transcript.includes('skip')) {
+  // } else if (coachFetchedCount > 2) {
     processed = skip(transcript);
   }
 
